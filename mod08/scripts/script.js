adef6f62f66c05fa8156c4c0cd9677ee9126fa23
mariohm <mariohm1989@gmail.com>
2015-04-04 02:49:41 +0200

    Añado obligatorio del módulo 8

--- mod08/scripts/script.js
+++ mod08/scripts/script.js
@@ -12,7 +12,7 @@ $(function(){
         tiempos.push(crono);
         localStorage.tiempos = JSON.stringify(tiempos); //Pasamos a JSON
         mostrarTiempos(tiempos);
-    };
+    }
 
     //Repintamos el div con la lista de los instantes de tiempo
     function mostrarTiempos(tiempos)
@@ -20,34 +20,35 @@ $(function(){
         var i, instantes = "";
         for(i = 0; i < tiempos.length; i++)
         {
-            instantes += "<p>Parada en el instante " + tiempos[i] + "</p>"
+            instantes += "<p>Parada en el instante " + tiempos[i] + "</p>";
         }
         $('#instantes').html(instantes);
-    };
+    }
 
-    function incr()	    { localStorage.c = +localStorage.c + 0.1;};
-    function mostrar()  { cl.html((+localStorage.c).toFixed(1)); };
+    function incr()	    { localStorage.c = +localStorage.c + 0.1;}
+    function mostrar()  { cl.html((+localStorage.c).toFixed(1)); }
     function arrancar()
     {
         $('#cambiar').attr('src', 'images/start.png');
         t=setInterval(function(){incr(); mostrar()}, 100);
-    };
+    }
     function parar()
     {
         $('#cambiar').attr('src', 'images/pause.png');
         clearInterval(t);
         actualizarLista();
         t=undefined;
-    };
-    function cambiar()  { if (!t) arrancar(); else parar(); };
+    }
+    function cambiar()  { if (!t) arrancar(); else parar(); }
 
     if(typeof window.ontouchstart !== 'undefined')
     {
-        //Evento táctil en todo el div principal
+        //Evento táctil en todo el body
         $("#todo").on('tap', cambiar);
         $("#todo").on('swipe', function(){
-            if(t == undefined)
+            if(t === undefined)
             {
+                $('#cambiar').attr('src', 'images/start.png');
                 localStorage.c = "0.0";
                 localStorage.tiempos = '[]';
                 tiempos = JSON.parse(localStorage.tiempos);
@@ -64,8 +65,9 @@ $(function(){
     {
         $("#cambiar").on('click', cambiar);
         $("#inicializar").on('click', function(){
-            if(t == undefined)
+            if(t === undefined)
             {
+                $('#cambiar').attr('src', 'images/start.png');
                 localStorage.c = "0.0";
                 localStorage.tiempos = '[]';
                 tiempos = JSON.parse(localStorage.tiempos);
