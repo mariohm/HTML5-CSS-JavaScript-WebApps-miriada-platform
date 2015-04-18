c7e712ccbe77694092114bae9cdac8fbdff2e9c2
mariohm <mariohm1989@gmail.com>
2015-04-18 15:37:02 +0200

    Añado obligatorio del módulo 10 y actualizo README. Fin del curso

--- README.md
+++ README.md
@@ -9,12 +9,21 @@
 #####Módulos
 
 * Módulo 1.- Introducción a los elementos básicos de HTML, CSS, codificación e hiperenlaces.
+
 * Módulo 2.- Estructura de la navegación y el texto en páginas Web HTML5, prioridad en CSS3.
-* Módulo 3.- Estructuración, visualización y depuración con Firefox de HTML5 y CSS3 adaptados a un entorno responsivo y multi-pantalla.
-* Módulo 4.- Introducción a JavaScript, sus sentencias, variables, expresiones, scripts de cliente.
-* Módulo 5.- Primeros programas JavaScript.
-* Módulo 6.- Objetos, propiedades, métodos, eventos, formularios, sentencia for/in y características avanzadas de objetos, incluyendo acceso a la página Web cargada en el navegador a través del árbol DOM.
-* Módulo 7.- Interacción con el usuario, eventos y sus manejadores, formularios, JQuery, Zepto, Arrays y bucles.
-* Módulo 8.- Ejemplo de un cronómetro. Eventos táctiles, localstorage y JSON, así como su utilización en aplicaciones Web.
-* Módulo 9.- Gráficos SVG y HTML5 CANVAS. Empaquetamiento de web apps para FirefoxOS.
-* Módulo 10.- API de geolocalización HTML5. Proyecto final del curso basado en Google Maps.
+
+* Módulo 3.- Estructuración, visualización y depuración con Firefox de HTML5 y CSS3 adaptados a un entorno responsivo y multi-pantalla. [Ejercicio opcional 1](http://mariohm.neocities.org/mod03/t2-opcional.html) [Ejercicio opcional 2](http://mariohm.neocities.org/mod03/t3-opcional.html)
+
+* Módulo 4.- Introducción a JavaScript, sus sentencias, variables, expresiones, scripts de cliente. [Ejercicio obligatorio](http://mariohm.neocities.org/mod04/obligatorio.html)
+
+* Módulo 5.- Primeros programas JavaScript. [Ejercicio opcional 1](http://mariohm.neocities.org/mod05/t2-opcional.html)
+
+* Módulo 6.- Objetos, propiedades, métodos, eventos, formularios, sentencia for/in y características avanzadas de objetos, incluyendo acceso a la página Web cargada en el navegador a través del árbol DOM. [Ejercicio opcional 1](http://mariohm.neocities.org/mod06/t5-opcional.html) [Ejercicio opcional 2](http://mariohm.neocities.org/mod06/t6-opcional.html) [Ejercicio obligatorio](http://mariohm.neocities.org/mod06/obligatorio.html)
+
+* Módulo 7.- Interacción con el usuario, eventos y sus manejadores, formularios, JQuery, Zepto, Arrays y bucles. [Ejercicio opcional 1](http://mariohm.neocities.org/mod07/t4-opcional.html) [Ejercicio opcional 2](http://mariohm.neocities.org/mod07/final-opcional.html)
+
+* Módulo 8.- Ejemplo de un cronómetro. Eventos táctiles, localstorage y JSON, así como su utilización en aplicaciones Web. [Ejercicio obligatorio](http://mariohm.neocities.org/mod08/obligatorio.html)
+
+* Módulo 9.- Gráficos SVG y HTML5 CANVAS. Empaquetamiento de web apps para FirefoxOS. [Ejercicio opcional 1](http://mariohm.neocities.org/mod09/t1-opcional.html) [Ejercicio opcional 2](http://mariohm.neocities.org/mod09/t2-opcional.html)
+
+* Módulo 10.- API de geolocalización HTML5. Proyecto final del curso basado en Google Maps. [Ejercicio opcional 1](http://mariohm.neocities.org/mod10/t2-opcional/hosted/t2-opcional.html) [Ejercicio obligatorio](http://mariohm.neocities.org/mod10/obligatorio/hosted/obligatorio.html)
---
+++ mod10/obligatorio/hosted/scripts/script.js
@@ -0,0 +1,186 @@
+$(function(){
+    var map, lat, lng;
+    // Dos variables para que, por cada 5 rutas, cada una sea de distinto color
+    var colors = ["#0099FF", "#00FF00", "#FFFF00", "#FF0000", "#00FFCC"];
+    var click = 0;
+    localStorage.locations = (localStorage.locations || '[]'); // Variable del navegador
+
+    var locations = JSON.parse(localStorage.locations); //Pasamos a array común
+
+    //Inicializar el mapa
+    $('#boton').click(function(e){
+        locations = JSON.parse(localStorage.locations); //Pasamos a array común
+
+        //geolocalizar(); No llamo a esto para no volver a pedir confirmación de permisos
+        map = new GMaps({  // muestra mapa centrado en coords [lat, lng]
+            el: '#map',
+            lat: locations[0][0],
+            lng: locations[0][1],
+            zoom: 15,
+            click: enlazarMarcador,
+            tap: enlazarMarcador
+        });
+
+        map.addMarker({ lat: locations[0][0], lng: locations[0][1]});  // marcador en [lat, lng]
+
+        locations = [locations[0]];
+        localStorage.locations = JSON.stringify(locations);
+
+        click = 0;
+
+    });
+    //Compactar la ruta
+    $('#boton2').click(function(e){
+
+        if(locations.length > 2)
+        {
+            locations = JSON.parse(localStorage.locations); //Pasamos a array común
+            click = 0;
+
+            map = new GMaps({  // muestra mapa centrado en coords [lat, lng]
+                el: '#map',
+                lat: locations[0][0],
+                lng: locations[0][1],
+                zoom: 15,
+                click: enlazarMarcador,
+                tap: enlazarMarcador
+            });
+
+            map.addMarker({ lat: locations[0][0], lng: locations[0][1]});  // marcador en [lat, lng]
+
+            map.drawRoute({
+                origin: [locations[0][0], locations[0][1]],  // origen del punto inicial
+                destination: [locations[locations.length - 1][0], locations[locations.length - 1][1]],
+                travelMode: 'driving',
+                strokeColor: colors[click],
+                strokeOpacity: 0.6,
+                strokeWeight: 5
+            });
+
+            lat = locations[locations.length - 1][0];
+            lng = locations[locations.length - 1][1];
+
+            locations = [locations[0]];
+            locations.push([lat, lng]);
+
+            map.addMarker({ lat: lat, lng: lng});  // pone marcador en mapa
+            click++;
+        }
+        else
+        {
+            alert("Solo podemos reducir una ruta con más de 2 puntos");
+        }
+
+    });
+
+    function recordarRuta()
+    {
+        if(locations.length > 1)
+        {
+            var i = 0;
+            click = 0;
+
+            map = new GMaps({  // muestra mapa centrado en coords [lat, lng]
+                el: '#map',
+                lat: locations[i][0],
+                lng: locations[i][1],
+                zoom: 15,
+                click: enlazarMarcador,
+                tap: enlazarMarcador
+            });
+            map.addMarker({ lat: locations[i][0], lng: locations[i][1]});  // pone marcador en mapa
+
+            for(i = 0; i < locations.length - 1; i++)
+            {
+                map.drawRoute({
+                    origin: [locations[i][0], locations[i][1]],  // origen en coordenadas anteriores
+                    // destino en coordenadas del click o toque actual
+                    destination: [locations[i + 1][0], locations[i + 1][1]],
+                    travelMode: 'driving',
+                    strokeColor: colors[click],
+                    strokeOpacity: 0.6,
+                    strokeWeight: 5
+                });
+
+                lat = locations[i + 1][0];   // guarda coords para marca siguiente
+                lng = locations[i + 1][1];
+
+                map.addMarker({ lat: lat, lng: lng});  // pone marcador en mapa
+                click++;
+                // Reset a contador
+                if(click > 4)
+                {
+                    click = 0;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    function enlazarMarcador(e)
+    {
+        var ultimaPos = locations.length - 1;
+        // muestra ruta entre marcas anteriores y actuales
+        map.drawRoute({
+            origin: [locations[ultimaPos][0], locations[ultimaPos][1]],  // origen en coordenadas anteriores
+            // destino en coordenadas del click o toque actual
+            destination: [e.latLng.lat(), e.latLng.lng()],
+            travelMode: 'driving',
+            strokeColor: colors[click],
+            strokeOpacity: 0.6,
+            strokeWeight: 5
+        });
+
+        lat = e.latLng.lat();   // guarda coords para marca siguiente
+        lng = e.latLng.lng();
+
+        locations.push([lat, lng]);
+        localStorage.locations = JSON.stringify(locations); //Pasamos a JSON
+
+        map.addMarker({ lat: lat, lng: lng});  // pone marcador en mapa
+        click++;
+        // Reset a contador
+        if(click > 4)
+        {
+            click = 0;
+        }
+    }
+
+    function geolocalizar()
+    {
+        if(!recordarRuta())
+        {
+            GMaps.geolocate({
+                success: function(position){
+                    lat = position.coords.latitude;  // guarda coords en lat y lng
+                    lng = position.coords.longitude;
+
+                    locations.push([lat, lng]);
+                    localStorage.locations = JSON.stringify(locations); //Pasamos a JSON
+
+                    map = new GMaps({  // muestra mapa centrado en coords [lat, lng]
+                        el: '#map',
+                        lat: lat,
+                        lng: lng,
+                        zoom: 15,
+                        click: enlazarMarcador,
+                        tap: enlazarMarcador
+                    });
+
+                    map.addMarker({ lat: lat, lng: lng});  // marcador en [lat, lng]
+
+
+                },
+                error: function(error) { alert('Error: '+error.message);},
+                not_supported: function(){ alert("No soporta geolocalización");}
+            });
+
+        }
+
+    }
+    
+    geolocalizar();
+});
